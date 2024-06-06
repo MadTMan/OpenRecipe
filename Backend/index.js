594d22ca6dc8769ece00e6fde8c720af37bf9ced
MadTMan <smtoufique.000@gmail.com>
2024-06-07 01:22:31 +0600

    First Successful Get Request

--- Backend/index.js
+++ Backend/index.js
@@ -14,6 +14,37 @@ app.get('/', (req, res) => {
     return res.status(234).send('Recipe APP');
 });
 
+//  Route to get recipe from database
+app.get('/recipes', async(req, res) => {
+    try{
+        const recipes = await Recipe.find({}); 
+
+        return res.status(200).json({
+            count: recipes.length,
+            data: recipes
+        });
+    } catch(error){
+        console.log(error.message);
+        res.status(500).send({message: error.message});
+    }
+});
+
+//  Route to get one recipe from database by id
+app.get('/recipes/:id', async(req, res) => {
+    try{
+        
+        const {id} = req.params;
+        
+        const recipe = await Recipe.findById(id); 
+
+        return res.status(200).json(recipe);
+    } catch(error){
+        console.log(error.message);
+        res.status(500).send({message: error.message});
+    }
+});
+
+
 //route to save a New Recipe
 app.post('/recipes', async (req, res) => {
     try {

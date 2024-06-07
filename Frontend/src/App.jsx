4ede34b33b7b8c54753740811d3d17e208d0506f
MadTMan <smtoufique.000@gmail.com>
2024-06-08 04:12:06 +0600

    Front end routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import CreateRecipe from './pages/CreateRecipe'
+import DeleteRecipe from './pages/DeleteRecipe'
+import ShowRecipe from './pages/ShowRecipe'
+import EditRecipe from './pages/EditRecipe'
 
  const App = () => {
   return (
-    <div className='bg-red-700 text-white'>App</div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/recipes/create' element={<CreateRecipe />} />
+        <Route path='/recipes/delete/:id' element={<DeleteRecipe />} />
+        <Route path='/recipes/details/:id' element={<ShowRecipe />} />
+        <Route path='/recipes/edit/:id' element={<EditRecipe />} />
+      </Routes>
   )
 }
 export default App
